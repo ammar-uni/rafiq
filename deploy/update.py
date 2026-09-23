7f7e8f62a90e097b09f6e2144342d38203680036
ammar-uni <283319294+ammar-uni@users.noreply.github.com>
2026-09-23 04:32:22 +0200

    Require reviewed migration to schema 6

--- deploy/update.py
+++ deploy/update.py
@@ -23,7 +23,7 @@ BASE = Path('/opt/rafiq')
 RELEASES = BASE / 'releases'
 META = Path('/var/opt/rafiq-deploy')
 STATUS = Path('/run/rafiq/status.json')
-SCHEMA = '5'
+SCHEMA = '6'
 
 
 def run(*args, **kwargs):
